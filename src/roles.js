e9d3dc60e448cce3a430431b318abfbb0946df5b
Jeks12 <mrjekshacks12@gmail.com>
2024-11-23 17:51:37 +0800

    Convert all files to CommonJS syntax

--- src/roles.js
+++ src/roles.js
@@ -1,9 +1,17 @@
-import AccessControl from 'accesscontrol';
+const AccessControl = require('accesscontrol');
 const ac = new AccessControl();
 
 exports.roles = (function () {
-  ac.grant('user').readAny('product');
-  ac.grant('admin').extend('user').createAny('product');
+  ac.grant('user')
+    .readAny('product')
+    .readOwn('order');
+
+  ac.grant('admin')
+    .extend('user')
+    .createAny('product')
+    .updateAny('product')
+    .deleteAny('product')
+    .readAny('order');
 
   return ac;
 })();
